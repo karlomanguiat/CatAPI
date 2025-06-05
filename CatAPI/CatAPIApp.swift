1bc7978ed741e1df24c2d8be5b1ea334eb075899
Glenn Manguiat <karlo.manguiat@gmail.com>
2025-06-05 16:43:55 +0800

    Initial Commit

---
+++ CatAPI/CatAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIApp.swift
+//  CatAPI
+//
+//  Created by Glenn Karlo Manguiat on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatAPI
+//
+//  Created by Glenn Karlo Manguiat on 6/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
